ab02709ad9972b3f547b07aa8d115713c93d4bd1
Steven <52871164+sstonaker@users.noreply.github.com>
2023-01-30 08:13:06 -0600

    working through examples

--- Two Pointers/Problem1.py
+++ Two Pointers/Problem1.py
@@ -19,11 +19,11 @@ Explanation: Both the quadruplets add up to the target
 def search_quadruplets(arr, target):
     arr.sort()
     quadruplets = []
-    for i in range(0, len(arr)-3):
+    for i in range(0, len(arr) - 3):
         # skip same element to avoid duplicate quadruplets
         if i > 0 and arr[i] == arr[i - 1]:
             continue
-        for j in range(i + 1, len(arr)-2):
+        for j in range(i + 1, len(arr) - 2):
             # skip same element to avoid duplicate quadruplets
             if j > i + 1 and arr[j] == arr[j - 1]:
                 continue
@@ -34,16 +34,15 @@ def search_quadruplets(arr, target):
 def search_pairs(arr, target_sum, first, second, quadruplets):
     left = second + 1
     right = len(arr) - 1
-    while (left < right):
+    while left < right:
         quad_sum = arr[first] + arr[second] + arr[left] + arr[right]
         if quad_sum == target_sum:  # found the quadruplet
-            quadruplets.append(
-                [arr[first], arr[second], arr[left], arr[right]])
+            quadruplets.append([arr[first], arr[second], arr[left], arr[right]])
             left += 1
             right -= 1
-            while (left < right and arr[left] == arr[left - 1]):
+            while left < right and arr[left] == arr[left - 1]:
                 left += 1  # skip same element to avoid duplicate quadruplets
-            while (left < right and arr[right] == arr[right + 1]):
+            while left < right and arr[right] == arr[right + 1]:
                 right -= 1  # skip same element to avoid duplicate quadruplets
         elif quad_sum < target_sum:
             left += 1  # we need a pair with a bigger sum
---
+++ Two Pointers/removeduplicates.py
@@ -0,0 +1,32 @@
+"""Problem Statement
+
+Given an array of sorted numbers, remove all duplicate number instances from it in-place, such that each element appears only once. The relative order of the elements should be kept the same and you should not use any extra space so that that the solution have a space complexity of O(1).
+
+Move all the unique elements at the beginning of the array and after moving return the length of the subarray that has no duplicate in it.
+
+Example 1:
+
+Input: [2, 3, 3, 3, 6, 9, 9]
+Output: 4
+Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+Example 2:
+
+Input: [2, 2, 2, 11]
+Output: 2
+Explanation: The first two elements after removing the duplicates will be [2, 11]."""
+
+
+def remove_duplicates(arr):
+    non_dup, i = 1, 0
+    while i < len(arr):
+        print((non_dup), i)
+        if arr[non_dup - 1] != arr[i]:
+            arr[non_dup] = arr[i]
+            non_dup += 1
+        i += 1
+
+    return non_dup
+
+
+print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+print(remove_duplicates([2, 2, 2, 11]))
